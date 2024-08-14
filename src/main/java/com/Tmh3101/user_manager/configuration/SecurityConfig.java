707cd61e1b48352b92f2dea40f81cdf197677874
Tmh3101 <minhhieu31012004@gmail.com>
2024-08-14 18:00:20 +0700

    add auth token to send request

--- src/main/java/com/Tmh3101/user_manager/configuration/SecurityConfig.java
+++ src/main/java/com/Tmh3101/user_manager/configuration/SecurityConfig.java
@@ -1,25 +1,34 @@
 package com.Tmh3101.user_manager.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-
-        final String[] PUBLIC_ENDPOINT = {
+    private final String[] PUBLIC_ENDPOINT = {
             "/users/add",
             "/auth/token",
             "/auth/introspect"
-        };
+    };
+
+    @Value("${jwt.signerKey}")
+    private String signerKey;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
 
         // authenticate path '/users/add'
         httpSecurity.authorizeHttpRequests(request ->
@@ -27,9 +36,23 @@ public class SecurityConfig {
                     .anyRequest().authenticated()
         );
 
+        // authenticate with token
+        httpSecurity.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
+        );
+
         // disable csrf configuration - csrf is configured automatically
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
     }
 }
